f10d9b271f6bc23fe2239bb03bd2b83ed786dc5e
Ian Miller <irmiller22@gmail.com>
2014-10-27 11:18:46 -0400

    added solution

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,8 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+  prime = input
+  (2..Math.sqrt(input).to_i).each do |i|
+    break if prime <= 1
+    prime /= i while (prime > i && prime % i == 0)
+  end
+  prime
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_reader :number
+
+  def initialize(input)
+    @number = largest_prime_factor(input)
+  end
+
+  def largest_prime_factor(input)
+    prime = input
+    (2..Math.sqrt(input).to_i).each do |i|
+      break if prime <= 1
+      prime /= i while (prime > i && prime % i == 0)
+    end
+    prime
+  end
+end
+
